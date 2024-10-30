8bae7d8bae483d4a93b188fc04d277c983ccba73
vishnu <vishnukg836@gmail.com>
2024-10-30 20:15:59 +0530

    filxe error

--- init_setup.sh
+++ init_setup.sh
@@ -1,8 +1,8 @@
 echo [$(date)]: "START"
-echo [$(date)]: "Creating conda env with python 3.8"
-conda create --prefix ./env python=3.8 -y
-echo [$(date)]: "activate env"
-source activate ./env
-echo [$(date)]: "intalling dev requirements"
+echo [$(date)]: "Creating Python virtual environment with python 3.8"
+python -m venv env  # Creates a virtual environment named "env"
+echo [$(date)]: "Activating virtual environment"
+source env/Scripts/activate  # Corrected path
+echo [$(date)]: "Installing dev requirements"
 pip install -r requirements_dev.txt
 echo [$(date)]: "END"
